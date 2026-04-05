010132b5a71f7db165e629e9070c349c1f5d9ca6
atharvak <atharvakadam631@gmail.com>
2026-04-05 14:00:37 +0530

    deployment fix

--- risingup_backend/settings.py
+++ risingup_backend/settings.py
@@ -130,9 +130,12 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
+        "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT", "3306"),
-        "OPTIONS": {"charset": "utf8mb4"},
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
