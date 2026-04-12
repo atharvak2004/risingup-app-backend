6c366b0f80f9dd087b7aa5297703b2f0436d78fc
atharvak <atharvakadam631@gmail.com>
2026-04-12 11:08:10 +0530

    updated is first login to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,16 +6,16 @@ from .models import User, StudentProfile, StaffProfile
 class UserAdmin(BaseUserAdmin):
     model = User
 
-    # Fields shown in admin list
-    list_display = ("id", "username", "email", "role", "is_staff", "is_active")
-    list_filter = ("role", "is_staff", "is_active")
+    list_display = ("id", "username", "email", "role", "is_first_login", "is_staff", "is_active")
 
-    # Fieldsets (edit page)
+    list_editable = ("is_first_login",)
+
+    list_filter = ("role", "is_staff", "is_active", "is_first_login")
+
     fieldsets = BaseUserAdmin.fieldsets + (
         ("Custom Fields", {"fields": ("role", "school", "phone", "is_first_login")}),
     )
 
-    # Add user form
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
         ("Custom Fields", {"fields": ("role", "school", "phone")}),
     )
